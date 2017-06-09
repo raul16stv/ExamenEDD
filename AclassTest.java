1b4afb6afd9dd5f8c5784d09cbdf79905618bd98
raul16stv <raulmartinezzgz@gmail.com>
2017-06-09 11:45:31 +0200

    Mockito2Test
    Hecho

--- AclassTest.java
+++ AclassTest.java
@@ -1,13 +1,24 @@
 import static org.junit.Assert.*;
 
+import java.awt.Button;
 import java.awt.Point;
 import java.awt.PointerInfo;
 
-import org.junit.Test;
+import javax.swing.JFrame;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+@RunWith(org.mockito.junit.MockitoJUnitRunner.class)
 public class AclassTest {
 	Aclass a= new Aclass();
-
+	@Mock
+	Aclass b;
+	@Mock
+	Button boton;
+	@Mock
+	JFrame frame;
 	@Test
 	public void concatTest(){
 		String esperado1="hola";
@@ -30,13 +41,33 @@ public class AclassTest {
 	public void locateTest(){
 		int x=1;
 		int y=2;
-		double esperado=(1.2);
-		assertEquals(0, a.locate(x, y));
+		int x1=100;
+		int y1=200;
+		
+		Point esperado= new Point(1, 2);
+		Point esperado2= new Point(100, 200);
+		Point casodecero= new Point(0,0);
+		
+		assertEquals(esperado, a.locate(x, y));
+		assertEquals(esperado2, a.locate(x1, y1));
 		try {
-			assertEquals(1, a.locate(0, 0));
+			assertEquals(casodecero, a.locate(0, 0));
 		} catch (Exception e) {
-			// TODO: handle exception
+
 		}
 	}
+	
+	public void Mockito1Test(){
+	Button boton;
+	Point punto= new Point(10, 10);
+	
+	
+	
+	}
+	public void Mockito2Test(){
+		Mockito.when(b.mockito2(frame, boton)).thenReturn(1);
+		
+	}
+	}
 
-}
+
